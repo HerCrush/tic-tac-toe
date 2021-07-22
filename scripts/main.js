c38254942011372dd55d52f8b7942656a9415369
nacho <nacholeon30@hotmail.com>
2021-07-22 17:21:10 -0300

    game now working

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,8 @@
 const gameBoard = (() => {
+
     let gameboard = [];
+    gameboard.length = 9;
+
     const displayGameBoard = () => {
         const board = document.createElement('div');
         board.classList.add('board');
@@ -13,24 +16,36 @@ const gameBoard = (() => {
         }
         document.querySelector('#container').appendChild(board);
     };
+
     return {gameboard, displayGameBoard};
 })();
 
 const player = () => {
+
+    let name;
+
     let marker;
-    return {marker};
+
+    return {name, marker};
 };
 
 const game = (() => {
+
     let turnPlayer;
+
     const player1 = player();
+
     const player2 = player();
+
     const start = () => {
         playBtn.classList.toggle('hidden');
         gameBoard.displayGameBoard();
         player1.marker = 'X';
         player2.marker = 'O';
-    }
+        player1.name = prompt('Player 1 name:');
+        player2.name = prompt('Player 2 name:');
+    };
+
     const playRound = () => {
         if(player1 === turnPlayer) {
             turnPlayer = player2;
@@ -38,14 +53,68 @@ const game = (() => {
         else {
             turnPlayer = player1;
         }
-    }
+    };
+
     const play = (e) => {
         const selectedBox = document.querySelector(`div[data-box="${e.target.dataset.box}"]`);
         if(selectedBox.textContent === player1.marker || selectedBox.textContent === player2.marker) return;
         playRound();
-        selectedBox.textContent = turnPlayer.marker;
-        console.log(e.target.dataset.box);
+        selectedBox.textContent = gameBoard.gameboard[e.target.dataset.box-1] = turnPlayer.marker;
+        if(isGameOver()) gameOver();
+        if(isATie()) tie();
     };
+
+    const isGameOver = () => {
+        if((gameBoard.gameboard[0]===turnPlayer.marker && gameBoard.gameboard[1]===turnPlayer.marker && gameBoard.gameboard[2]===turnPlayer.marker)||
+            (gameBoard.gameboard[3]===turnPlayer.marker && gameBoard.gameboard[4]===turnPlayer.marker && gameBoard.gameboard[5]===turnPlayer.marker)||
+            (gameBoard.gameboard[6]===turnPlayer.marker && gameBoard.gameboard[7]===turnPlayer.marker && gameBoard.gameboard[8]===turnPlayer.marker)||
+            (gameBoard.gameboard[0]===turnPlayer.marker && gameBoard.gameboard[3]===turnPlayer.marker && gameBoard.gameboard[6]===turnPlayer.marker)||
+            (gameBoard.gameboard[1]===turnPlayer.marker && gameBoard.gameboard[4]===turnPlayer.marker && gameBoard.gameboard[7]===turnPlayer.marker)||
+            (gameBoard.gameboard[2]===turnPlayer.marker && gameBoard.gameboard[5]===turnPlayer.marker && gameBoard.gameboard[8]===turnPlayer.marker)||
+            (gameBoard.gameboard[0]===turnPlayer.marker && gameBoard.gameboard[4]===turnPlayer.marker && gameBoard.gameboard[8]===turnPlayer.marker)||
+            (gameBoard.gameboard[2]===turnPlayer.marker && gameBoard.gameboard[4]===turnPlayer.marker && gameBoard.gameboard[6]===turnPlayer.marker)) {
+            return true;
+        }
+        return false;
+    };
+
+    const isATie = () => {
+        if(gameBoard.gameboard.includes(undefined)) return false;
+        return true;
+    };
+
+    const gameOver = () => {
+        const gameBoardDisabler = document.createElement('div');
+        gameBoardDisabler.id = 'game_board_disabler';
+        const gameOverSign = document.createElement('div');
+        gameOverSign.id = 'game_over';
+        const para1 = document.createElement('p');
+        para1.id = 'game_over_text';
+        para1.textContent = "GAME OVER!";
+        const para2 = document.createElement('p');
+        para2.id = 'game_over_tie';
+        para2.textContent = `${turnPlayer.name} wins!`;
+        gameOverSign.append(para1, para2);
+        gameBoardDisabler.appendChild(gameOverSign);
+        document.querySelector('.board').appendChild(gameBoardDisabler);
+    };
+    
+    const tie = () => {
+        const gameBoardDisabler = document.createElement('div');
+        gameBoardDisabler.id = 'game_board_disabler';
+        const gameOverSign = document.createElement('div');
+        gameOverSign.id = 'game_over';
+        const para1 = document.createElement('p');
+        para1.id = 'game_over_text';
+        para1.textContent = "GAME OVER!";
+        const para2 = document.createElement('p');
+        para2.id = 'game_over_winner';
+        para2.textContent = `It's a tie!`;
+        gameOverSign.append(para1, para2);
+        gameBoardDisabler.appendChild(gameOverSign);
+        document.querySelector('.board').appendChild(gameBoardDisabler);
+    };
+
     return {start, play};
 })();
 
